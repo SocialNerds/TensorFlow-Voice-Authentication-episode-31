14ebd7d25118933b6b6d2f2ba1ac884f164e0619
matrixlord <th.nokas@gmail.com>
2017-11-21 23:59:53 +0200

    Saving FFT numpy arrays for faster response

--- data/scripts/files.py
+++ data/scripts/files.py
@@ -3,8 +3,6 @@ import scipy.fftpack
 from scipy.io import wavfile
 from os import listdir
 import random
-import sqlite3
-import pickle
 
 thanos_path = 'files/first'
 thanos_other_path = 'files/second'
@@ -12,29 +10,12 @@ test_path = 'files/test'
 
 # Get a specific file.
 def get_file_data(file_path):
-    # fs, data = wavfile.read(file_path)
-    # print('FFT: ', file_path)
-    # N = 2000
-    # data = scipy.fftpack.fft(data)
-    # data = np.abs(data[:N//2])
-    # data = 2*(data - np.max(data))/-np.ptp(data)-1
-    # return data
-    table_name = 'fourier_data'
-    database = 'data'
-    # Connect to DB.
-    conn = sqlite3.connect(table_name)
-    c = conn.cursor()
-    # Create table if it does not exist.
-    sql = 'create table if not exists ' + table_name + ' (file_path text, data text)'
-    c.execute(sql)
-    conn.commit()
-    
-    c.execute('SELECT data FROM ' + table_name + ' WHERE file_path = ? ', (file_path,))
-    data = c.fetchone()
-    if data != None:
-        conn.close()
-        return pickle.loads(data)
-    else:
+    try:
+        # Get if pickled file, already exists.
+        data = np.load(file_path + '_np.npy')
+        return data
+    except IOError:
+        # Save pickled file.
         fs, data = wavfile.read(file_path)
         print('FFT: ', file_path)
         N = 2000
@@ -42,10 +23,8 @@ def get_file_data(file_path):
         data = np.abs(data[:N//2])
         data = 2*(data - np.max(data))/-np.ptp(data)-1
 
-        # Save to DB.
-        c.execute('insert into ' + table_name + '(file_path, data) values (?, ?)', (file_path, pickle.dumps(data, protocol=0)))
-        conn.commit()
-        conn.close()
+        # Save.
+        np.save(file_path + '_np', data)
         return data
 
 # Get all files from path.
