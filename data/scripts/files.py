5177274bd1e88c7501e256ce05032dfaaafc86d6
matrixlord <th.nokas@gmail.com>
2017-11-21 23:43:24 +0200

    Trying to serialize numpy arrays

--- data/scripts/files.py
+++ data/scripts/files.py
@@ -3,6 +3,8 @@ import scipy.fftpack
 from scipy.io import wavfile
 from os import listdir
 import random
+import sqlite3
+import pickle
 
 thanos_path = 'files/first'
 thanos_other_path = 'files/second'
@@ -10,16 +12,41 @@ test_path = 'files/test'
 
 # Get a specific file.
 def get_file_data(file_path):
-    fs, data = wavfile.read(file_path)
-    print('FFT: ', file_path)
-    # data_normalized = [(i/2**8.) * 2-1 for i in data]
-    # data_fft = fft(data_normalized)
-    # return abs(data_fft[:(40000)])
-    N = 2000
-    data = scipy.fftpack.fft(data)
-    data = np.abs(data[:N//2])
-    data = 2*(data - np.max(data))/-np.ptp(data)-1
-    return data
+    # fs, data = wavfile.read(file_path)
+    # print('FFT: ', file_path)
+    # N = 2000
+    # data = scipy.fftpack.fft(data)
+    # data = np.abs(data[:N//2])
+    # data = 2*(data - np.max(data))/-np.ptp(data)-1
+    # return data
+    table_name = 'fourier_data'
+    database = 'data'
+    # Connect to DB.
+    conn = sqlite3.connect(table_name)
+    c = conn.cursor()
+    # Create table if it does not exist.
+    sql = 'create table if not exists ' + table_name + ' (file_path text, data text)'
+    c.execute(sql)
+    conn.commit()
+    
+    c.execute('SELECT data FROM ' + table_name + ' WHERE file_path = ? ', (file_path,))
+    data = c.fetchone()
+    if data != None:
+        conn.close()
+        return pickle.loads(data)
+    else:
+        fs, data = wavfile.read(file_path)
+        print('FFT: ', file_path)
+        N = 2000
+        data = scipy.fftpack.fft(data)
+        data = np.abs(data[:N//2])
+        data = 2*(data - np.max(data))/-np.ptp(data)-1
+
+        # Save to DB.
+        c.execute('insert into ' + table_name + '(file_path, data) values (?, ?)', (file_path, pickle.dumps(data, protocol=0)))
+        conn.commit()
+        conn.close()
+        return data
 
 # Get all files from path.
 def get_files_from_path(folder_path):
